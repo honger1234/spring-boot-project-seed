fab97656993ec80820eeefc61d36500e712839d4
zt <183162113@qq.com>
2020-07-26 19:59:29 +0800

    初始化项目

---
+++ src/main/java/com/honger1234/springbootprojectseed/SpringBootProjectSeedApplication.java
@@ -0,0 +1,13 @@
+package com.honger1234.springbootprojectseed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectSeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProjectSeedApplication.class, args);
+    }
+
+}
